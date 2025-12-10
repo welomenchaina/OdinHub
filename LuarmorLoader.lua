e0cda817c79e21f4280722193d2c169ff16c64db
Hydrox <139654803+welomenchaina@users.noreply.github.com>
2025-12-10 09:40:33 -0600

    Add LuarmorLoader script for game validation
    This script checks if the game is loaded and validates the game ID before loading the appropriate script based on the game.

---
+++ LuarmorLoader.lua
@@ -0,0 +1,21 @@
+repeat task.wait() until game:IsLoaded()
+
+local cloneref = cloneref or function(v) return v end
+local Players = cloneref(game:GetService("Players"))
+local Plr = Players.LocalPlayer
+
+if not script_key then
+  Plr:Kick("You haven't entered a key yet or your executor is trash!")
+  return
+end
+
+local validgame
+
+if game.GameId == 7884563721 then
+  validgame = true
+  loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/5d931a534e183e0469eb5e92b8b3f52b.lua"))() -- arcade basketball
+end
+
+if not validgame then
+  loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/f6a92e7f0cc4fcec3184b7402d4232b8.lua"))() -- universal script
+end
