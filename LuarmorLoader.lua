42d62291d2d93b949e3e047c93428d6e49c90f1c
Hydrox <139654803+welomenchaina@users.noreply.github.com>
2025-12-12 18:00:47 -0600

    Update LuarmorLoader.lua

--- LuarmorLoader.lua
+++ LuarmorLoader.lua
@@ -16,6 +16,10 @@ if game.GameId == 7884563721 then
   loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/5d931a534e183e0469eb5e92b8b3f52b.lua"))() -- arcade basketball
 end
 
+if game.GameId == 184199275 then
+  loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/a3f402ada01f234399ea335aae31f420.lua"))()
+end
+
 if not validgame then
   loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/f6a92e7f0cc4fcec3184b7402d4232b8.lua"))() -- universal script
 end
